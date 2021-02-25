9615929403f7b8cf1600eda7d351ebefceab9c56
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2021-02-25 06:05:13 -0300

    25-02-2021

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -15,17 +15,30 @@ export default class App extends Component {
   state = {...initialState};
 
   addDigit = (n) => {
+    //Comando para realizar deploy
+    //console.debug(typeof this.state.displayValue);
+
+    //clear display é utilizado para definir se na próxima operação o display deve ser limpo
     const clearDisplay =
       this.state.displayValue === '0' || this.state.clearDisplay;
 
+    //Caso o digito a ser inserido seja '.' checamos se o clearDisplay está setado ou se já existe um ponto
     if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
       return;
     }
 
+    //currentValue recebe o valor disponível no display caso clearDisplay seja false
     const currentValue = clearDisplay ? '' : this.state.displayValue;
-    const displayValue = currentValue + n;
+    const displayValue = currentValue + n; //displayValue recebe o currentValue acima + o novo valor digitado
     this.setState({displayValue, clearDisplay: false});
 
+    /**
+     * Caso o digito não seja um ponto realizamos a inserção dos valores no array values
+     * newValue recebe um parseFloat do valor contido em displayValue armazenado acima
+     * values é um array que recebe os valores de state.values
+     * values[current] - current no começo é zero, porém ao realizarmos uma operação seu valor será
+     * alterado para 1, dessa forma conseguiremos ter um vetor com duas posições preenchidas
+     */
     if (n !== '.') {
       const newValue = parseFloat(displayValue);
       const values = [...this.state.values];
@@ -35,9 +48,16 @@ export default class App extends Component {
   };
 
   clearMemory = () => {
+    //reseta os estados para os valores iniciais
     this.setState({...initialState});
   };
 
+  /**
+   * Set operation é responsável por lidar com as operações inseridas
+   * primeiramente checamos se o current é igual a 0, caso sim setamos a operation passada como parametro
+   * mudamos o current para 1 dessa forma o próximo valor a ser inserido será armazenado na segunda posição da calculadora
+   * e o clearDisplay para true
+   */
   setOperation = (operation) => {
     if (this.state.current === 0) {
       this.setState({operation, current: 1, clearDisplay: true});
@@ -45,6 +65,7 @@ export default class App extends Component {
       const equals = operation === '=';
       const values = [...this.state.values];
       try {
+        //eval realiza o cálculo de expressões matemáticas
         values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
       } catch (e) {
         values[0] = this.state.values[0];
