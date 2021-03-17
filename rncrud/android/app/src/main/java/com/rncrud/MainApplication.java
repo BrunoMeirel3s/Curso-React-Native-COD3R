5c6c19bbc19875da4f73b2b8ce04960982624103
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2021-03-16 22:02:53 -0300

    16-03-2021

--- rncrud/android/app/src/main/java/com/rncrud/MainApplication.java
+++ rncrud/android/app/src/main/java/com/rncrud/MainApplication.java
@@ -1,11 +1,9 @@
-package com.navegacao;
+package com.rncrud;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -64,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.navegacao.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.rncrud.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
