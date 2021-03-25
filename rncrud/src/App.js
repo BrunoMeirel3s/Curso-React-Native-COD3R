6038f95a714647f4530971e96b5101f5b1f9af32
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2021-03-25 20:23:26 -0300

    25-03-2021

---
+++ rncrud/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native'; //Container responsável por permitir usar o react-navigation
+import {createStackNavigator} from '@react-navigation/stack'; //Navegação Stack, em forma de pilha
+import UserList from './views/UserList';
+import UserForm from './views/UserForm';
+import {Button, Icon} from 'react-native-elements';
+
+const Stack = createStackNavigator();
+
+export default props => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        {/**
+         * Para realizar a navegação entre páginas precisamos utilizar o método options
+         * desestruturando o navigation para então usá-lo no onPress do Button.
+         *
+         * O options está retornando um objeto contendo o título o elemento headerRight é utilizado
+         * para chamar o botão no canto direito
+         */}
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({navigation}) => {
+            return {
+              title: 'Lista de Usuários',
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('UserForm')}
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: 'Formulário de Usuários',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitlestyle: {
+    fontWeight: 'bold',
+  },
+};
---
+++ rncrud/src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2021/03/03/08/39/woman-6064799_960_720.jpg',
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar',
+    email: 'rimasil@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+  },
+  {
+    id: 3,
+    name: 'Rogério Fragoso',
+    email: 'fragoso@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2013/07/12/13/28/business-man-147092_960_720.png',
+  },
+  {
+    id: 4,
+    name: 'Rafael Monteiro',
+    email: 'rafamonte@email.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
+  },
+];
