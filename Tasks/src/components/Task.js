664bb6f6e0efb1993b31dc4ea75a10a30a0a09eb
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2021-04-13 21:36:37 -0300

    13-04-2021

---
+++ Tasks/src/components/Task.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import commonStyles from '../commonStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+export default props => {
+  /**
+   * Podemos utilizar condições para aplicar um estilo ou não,
+   * como neste exemplo criamos a constante doneOrNot que será aplicado a
+   * descrição da tarefa caso a tarefa já tenha sido executada
+   */
+  const doneOrNotStyle =
+    props.doneAt != null ? {textDecorationLine: 'line-through'} : {};
+
+  /**
+   * date receberá o valor de acordo com o que for passado como parametro para o component Task
+   */
+  const date = props.doneAt ? props.doneAt : props.estimateAt;
+
+  /**
+   * formattedDate é utilizado para formatar a data utilizado o moment
+   */
+  const formattedDate = moment(date)
+    .locale('pt-br')
+    .format('ddd, D [de] MMMM');
+
+  return (
+    <View style={styles.container}>
+      {/**
+       * checkContainer será a parte que terá o circulo que demonstra se a tarefa foi executada
+       * o componente será exibido com base na função getCheckView que irá checar se existe
+       * uma data de finalização da tarefa
+       */}
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <View>
+        <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+        <Text style={styles.date}>{formattedDate}</Text>
+      </View>
+    </View>
+  );
+};
+
+/**
+ * getCheckView irá chegar se foi passado a data de finalização da tarefa,
+ * com base nisso iremos renderizar o ícone de done da tarefa
+ */
+function getCheckView(doneAt) {
+  if (doneAt != null) {
+    return (
+      <View style={styles.done}>
+        <Icon name="check" size={20} color="#FFF" />
+      </View>
+    );
+  } else {
+    return <View style={styles.pending} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pending: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: '#4D7031',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  desc: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.mainText,
+    fontSize: 15,
+  },
+  date: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.subText,
+    fontSize: 12,
+  },
+});
--- Tasks/src/screens/TaskList.js
+++ Tasks/src/screens/TaskList.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
 import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import commonStyles from '../commonStyles';
 
 import todayImage from '../../assets/imgs/today.jpg';
 
+/**
+ * moment é importado aqui para ser utilizado na hora de trazer
+ * data e hora dentro da aplicação, importamos tamém o local pt-br
+ * para trazer a hora no nosso formato
+ */
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+import Task from '../components/Task';
+
 export default class TaskList extends Component {
   render() {
+    const today = moment()
+      .locale('pt-br')
+      .format('ddd, D [de] MMMM');
+
     return (
       <View style={styles.container}>
-        <ImageBackground
-          style={styles.background}
-          source={todayImage}></ImageBackground>
+        <ImageBackground style={styles.background} source={todayImage}>
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{today}</Text>
+          </View>
+        </ImageBackground>
         <View style={styles.taskList}>
-          <Text>TaskList</Text>
+          <Task
+            desc="Comprar Livro"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
+          <Task desc="Ler Livro" estimateAt={new Date()} doneAt={null} />
         </View>
       </View>
     );
@@ -27,5 +50,23 @@ const styles = StyleSheet.create({
   },
   taskList: {
     flex: 7,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
   },
 });
